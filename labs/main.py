6e8bd01abfead0642934040ab6c5ed32885fef91
kingboma2009-cell <kingboma2009@gmail.com>
2026-09-04 12:56:44 +0100

    Complete: A small shop wants to know how many of each item they have i

--- labs/main.py
+++ labs/main.py
@@ -1,8 +1,11 @@
-def remove_duplicates(items):
-    # TODO: use a loop to build a new list with duplicates removed, keeping first occurrences
-    pass
-    result = []
+def count_items(items):
+    # TODO: use a for loop to build a dictionary counting each item in `items`
+    counts = {}
     for item in items:
-        if item not in result:
-            result.append(item)
-    return result
+        if item in counts:
+            counts[item] += 1
+        else:
+            counts[item] = 1
+    return counts  
+
+    pass
