2d1a070bd3b785bfe37fc2578679fb880dc0ffaf
kingboma2009-cell <kingboma2009@gmail.com>
2026-09-04 13:13:10 +0100

    Complete: The function below is meant to take a list of sentences and

--- labs/main.py
+++ labs/main.py
@@ -1,9 +1,6 @@
-def list_average(numbers):
-    if not numbers:
-        return 0
+def total_word_count(sentences):
     total = 0
-    for p in numbers:
-        total += p 
-    return round(total / len(numbers), 2)
-    # TODO: use a for loop to calculate the average of `numbers`, rounded to 2 decimal places
-    pass
+
+    for sentence in sentences:   
+        total += len(sentence.split())
+    return total
