7d6b9964554881b70cb5cc7a7e669406df935d11
kingboma2009-cell <kingboma2009@gmail.com>
2026-09-09 13:58:25 +0100

    Complete: Below is a function meant to check whether a given word cont

--- labs/main.py
+++ labs/main.py
@@ -1,4 +1,10 @@
-def swap_tuple(pair):
-    return (pair[1], pair[0])
-    # TODO: return a new tuple with pair's two elements swapped
+def has_all_vowels(word):
+    required = {"a", "e", "i", "o", "u"}
+    word = word.lower()
+    for vowel in required:
+        if vowel not in word:
+            return False
+    return True
+    # TODO: build a set of vowels actually found in `word`,
+    # then check if it contains all of `required`
     pass
