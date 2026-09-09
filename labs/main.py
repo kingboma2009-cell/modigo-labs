6b7c755da2f7f7170fa627ed1cf16737fa09c740
kingboma2009-cell <kingboma2009@gmail.com>
2026-09-09 13:31:18 +0100

    Complete: Write a function that compares two people's hobby sets and r

--- labs/main.py
+++ labs/main.py
@@ -1,4 +1,8 @@
-def count_unique_visitors(visitors):
-    return len(set(visitors))
-    # TODO: convert `visitors` to a set to remove duplicates, then return its length
+def compare_hobbies(person1_hobbies, person2_hobbies):
+    return {
+        "shared": person1_hobbies & person2_hobbies,
+        "only_person1": person1_hobbies - person2_hobbies,
+        "only_person2": person2_hobbies - person1_hobbies
+    }
+    # TODO: use set operations to find shared, only_person1, and only_person2 hobbies
     pass
