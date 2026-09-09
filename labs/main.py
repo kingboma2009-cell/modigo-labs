0c47be7efa04ff458ef6474f174081c28d64c669
kingboma2009-cell <kingboma2009@gmail.com>
2026-09-09 13:20:41 +0100

    Complete: Below is a function meant to count how many distinct visitor

--- labs/main.py
+++ labs/main.py
@@ -1,4 +1,4 @@
-def same_point(point1, point2):
-    return point1 == point2
-    # TODO: return True if point1 and point2 represent the same location
+def count_unique_visitors(visitors):
+    return len(set(visitors))
+    # TODO: convert `visitors` to a set to remove duplicates, then return its length
     pass
