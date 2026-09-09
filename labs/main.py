7a729a74edc6b0750cea30ed4eebba6ede7efccc
kingboma2009-cell <kingboma2009@gmail.com>
2026-09-09 13:32:45 +0100

    Complete: The function below is meant to combine two sets of tags into

--- labs/main.py
+++ labs/main.py
@@ -1,8 +1,3 @@
-def compare_hobbies(person1_hobbies, person2_hobbies):
-    return {
-        "shared": person1_hobbies & person2_hobbies,
-        "only_person1": person1_hobbies - person2_hobbies,
-        "only_person2": person2_hobbies - person1_hobbies
-    }
-    # TODO: use set operations to find shared, only_person1, and only_person2 hobbies
-    pass
+def merge_tags(tags1, tags2): 
+    merged = tags1 | tags2
+    return merged
