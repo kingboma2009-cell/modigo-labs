a1c3e9f2eda86b7d99f98203d9601e8728b8124a
kingboma2009-cell <kingboma2009@gmail.com>
2026-09-11 10:38:56 +0100

    Complete: Fix the Tag Merger

--- challenges/main.py
+++ challenges/main.py
@@ -1,8 +1,3 @@
-def find_common_elements(list1, list2):
-    common = []
-    for item in list1:
-        if item in list2 and item not in common:
-            common.append(item)
-    return common        
-    # TODO: use for loops to find values present in both list1 and list2, with no duplicates
-    pass
+def merge_tags(tags1, tags2):
+    merged = tags1 | tags2
+    return merged
